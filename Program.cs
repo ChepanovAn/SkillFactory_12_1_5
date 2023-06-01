6d2761c355190fba7fc5f315515d4610ba1cb64c
Анатолий <Chepanov199287@gmail.com>
2023-06-01 20:42:03 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SkillFactory_12_1_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите: Логин \nИмя \nНаличие премиум подписки (Ответ: True либо False)");
+            string login = Console.ReadLine();
+            string name = Console.ReadLine();
+            bool subscription = Convert.ToBoolean(Console.ReadLine());
+
+            User Person = new User(login, name, subscription);
+            Console.WriteLine($"Привет {Person.Name}");
+
+            if (!Person.IsPremium)
+                User.ShowAds();
+        }
+    }
+}
